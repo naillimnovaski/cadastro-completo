94da8dc6231672fa16f1fedd8c5fab43a03ac4d7
naillim <naillim.domingues@alunoescolassociais.org.br>
2024-10-04 08:01:01 -0300

    adiciona validaçao de email

---
+++ cadastro.js
@@ -0,0 +1,8 @@
+'use strict';
+ 
+// Validação de Email
+const validarEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+};
+ 
